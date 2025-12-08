b0a96232a0f0b4028ab8e352fb044dd077ffda53
APOLLO <hungtruong11112005@gmail.com>
2025-12-09 00:35:05 +0700

    Edit file main.py

--- server/main.py
+++ server/main.py
@@ -1,23 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
 
-# Định nghĩa khuôn mẫu dữ liệu tin nhắn gửi lên
+# Bật CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class MessageRequest(BaseModel):
     text: str
 
 @app.post("/chat")
 async def chat_endpoint(request: MessageRequest):
     user_text = request.text
-    print(f"Nhận được tin nhắn: {user_text}") # In ra terminal để kiểm tra
-    
-    # Logic trả lời đơn giản (Sau này lắp AI vào đây)
+    print(f"Nhận được tin nhắn: {user_text}")
     bot_reply = f"Server đã nhận: '{user_text}'. Chào bạn!"
-    
     return {"reply": bot_reply}
 
 if __name__ == "__main__":
-    # Reload=True để sửa code xong nó tự cập nhật không cần tắt đi bật lại
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
